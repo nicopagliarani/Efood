d32fbf210d5424db3962d45fbeb3882a9b313cf8
nicopagliarani <98600157+nicopagliarani@users.noreply.github.com>
2022-03-19 15:59:26 +0100

    Style Profile page and Homepage kinda done, to improve

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,8 +1,14 @@
 const router = require("express").Router();
+const express = require("express");
+
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
+router.get("/profile", (req, res, next) => {
+  res.render("profile");
+});
+
 
 module.exports = router;
