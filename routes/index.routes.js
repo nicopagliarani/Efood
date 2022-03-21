5fa5610a43d9317efd3ec69dccc73f34caeeb9d6
nicopagliarani <98600157+nicopagliarani@users.noreply.github.com>
2022-03-21 18:26:56 +0100

    Code with Api

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,40 +1,25 @@
 const router = require("express").Router();
-<<<<<<< HEAD
 const axios = require("axios");
 const async = require("hbs/lib/async");
-=======
-const express = require("express");
-
->>>>>>> origin/nicolò
-
+const { RecipeSearchClient } = require("edamam-api");
+//hello
 /* GET home page */
 router.get("/", async (req, res, next) => {
   res.render("index");
-  // try {
-  //   const { RecipeSearchClient } = require("edamam-api");
-  //   const client = new RecipeSearchClient({
-  //     appId: `${process.env.CLIENT_ID}`,
-  //     appKey: `${process.env.CLIENT_SECRET}`,
-  //   });
-  //   console.log("here :", apiResponse.data);
-  //   res.render("index");
-  // } catch (err) {
-  //   console.log(err);
-  // }
 });
 
 router.post("/search", async (req, res) => {
-  let searchName = req.body.name;
-  searchName == "" ? (searchName = "chiken") : (searchName = req.body.name);
-  let responseApi = await axios.get(
-    `https://api.edamam.com/api/recipes/v2?type=public&q=${searchName}&app_id=ba8593df&app_key=66b63b0b31b038fb4db02ba8d6f8f9da&random=false&field=image`
-  );
+  //Calling the API
+  const client = new RecipeSearchClient({
+    appId: process.env.CLIENT_ID,
+    appKey: process.env.CLIENT_SECRET,
+  });
 
-  console.log(responseApi.data.hits[0].recipe);
+  const results = await client.search({ query: req.body.search }); //Search through the api
+  console.log(results.hits); //See where we are in the API
+
+  const ApiValues = results.hits; //Storing or position
+  res.render("SearchRecipes", { ApiValues }); //Send the api to the hbs to iterate over
 });
 
-
-
 module.exports = router;
-
-//let pathToImg = responseApi.data.hits[0].recipe;
