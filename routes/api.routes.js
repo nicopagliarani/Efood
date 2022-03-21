c928ac7f1ebd0bf847eb9ace08f73e2d3b727532
Adryan Cadet <adryan-pro@hotmail.com>
2022-03-21 16:22:09 +0100

    API res bug

--- routes/api.routes.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const axios = require("axios");
-
-// async function sendApiRequest() {
-//   const APP_ID = "ba8593df";
-//   const API_KEY = "66b63b0b31b038fb4db02ba8d6f8f9da";
-//   let response = await fetch("https://api.edamam.com");
-// }
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,39 +1,33 @@
 const router = require("express").Router();
-<<<<<<< HEAD
 const axios = require("axios");
 const async = require("hbs/lib/async");
-=======
-const express = require("express");
-
->>>>>>> origin/nicolò
+const { RecipeSearchClient } = require("edamam-api");
 
 /* GET home page */
 router.get("/", async (req, res, next) => {
   res.render("index");
-  // try {
-  //   const { RecipeSearchClient } = require("edamam-api");
-  //   const client = new RecipeSearchClient({
-  //     appId: `${process.env.CLIENT_ID}`,
-  //     appKey: `${process.env.CLIENT_SECRET}`,
-  //   });
-  //   console.log("here :", apiResponse.data);
-  //   res.render("index");
-  // } catch (err) {
-  //   console.log(err);
-  // }
+});
+
+// const recipeApi = new RecipeApi({
+//   clienId: process.env.CLIENT_ID,
+//   clienSecret: process.env.SESSION_SECRET,
+// });
+
+const client = new RecipeSearchClient({
+  appId: process.env.CLIENT_ID,
+  appKey: process.env.SESSION_SECRET,
 });
 
 router.post("/search", async (req, res) => {
-  let searchName = req.body.name;
-  searchName == "" ? (searchName = "chiken") : (searchName = req.body.name);
-  let responseApi = await axios.get(
-    `https://api.edamam.com/api/recipes/v2?type=public&q=${searchName}&app_id=ba8593df&app_key=66b63b0b31b038fb4db02ba8d6f8f9da&random=false&field=image`
-  );
-
-  console.log(responseApi.data.hits[0].recipe);
+  const results = await client.search({ query: "beef" });
+  // let searchName = req.body.name;
+  // searchName == "" ? (searchName = "beef") : (searchName = req.body.searchBar);
+  // let responseApi = await axios.get(
+  //   `https://api.edamam.com/api/recipes/v2?type=public&q=${searchName}&app_id=ba8593df&app_key=66b63b0b31b038fb4db02ba8d6f8f9da&random=false&field=image`
+  // );
+  console.log(results);
+  //console.log(responseApi.data.hits[0]._links);
 });
-
-
 
 module.exports = router;
 
