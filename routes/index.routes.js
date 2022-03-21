ffa78ad940b20e97d368f5263e77e41308c0ffcf
Adryan Cadet <adryan-pro@hotmail.com>
2022-03-21 18:00:35 +0100

    Undestand Api

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -8,27 +8,18 @@ router.get("/", async (req, res, next) => {
   res.render("index");
 });
 
-// const recipeApi = new RecipeApi({
-//   clienId: process.env.CLIENT_ID,
-//   clienSecret: process.env.SESSION_SECRET,
-// });
-
-const client = new RecipeSearchClient({
-  appId: process.env.CLIENT_ID,
-  appKey: process.env.SESSION_SECRET,
-});
-
 router.post("/search", async (req, res) => {
-  const results = await client.search({ query: "beef" });
-  // let searchName = req.body.name;
-  // searchName == "" ? (searchName = "beef") : (searchName = req.body.searchBar);
-  // let responseApi = await axios.get(
-  //   `https://api.edamam.com/api/recipes/v2?type=public&q=${searchName}&app_id=ba8593df&app_key=66b63b0b31b038fb4db02ba8d6f8f9da&random=false&field=image`
-  // );
-  console.log(results);
-  //console.log(responseApi.data.hits[0]._links);
+  //Calling the API
+  const client = new RecipeSearchClient({
+    appId: process.env.CLIENT_ID,
+    appKey: process.env.CLIENT_SECRET,
+  });
+
+  const results = await client.search({ query: req.body.search }); //Search through the api
+  console.log(results.hits); //See where we are in the API
+
+  const ApiValues = results.hits; //Storing or position
+  res.render("SearchRecipes", { ApiValues }); //Send the api to the hbs to iterate over
 });
 
 module.exports = router;
-
-//let pathToImg = responseApi.data.hits[0].recipe;
